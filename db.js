2766d9aa0b1c84ae35f77a7002928265c688e676
Dipankar chetia <dipankar.chetia@salesbook.in>
2024-07-27 21:45:45 +0530

    spacing issue

--- db.js
+++ db.js
@@ -1,29 +1,24 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const MongoURLServer =process.env.MongoURLServer ; ;
-const MongoURLLocal =process.env.MongoURLLocal ;
-
-
-
+const MongoURLServer = process.env.MongoURLServer;
+const MongoURLLocal = process.env.MongoURLLocal;
 
 mongoose.connect(MongoURLServer, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const db =mongoose.connection;
-console.log(db)
+const db = mongoose.connection;
 
-db.on("connected",()=>{
-    console.log("db is connected")
-})
-db.on("error",(error)=>{
-    console.log(`db is getting ${error}`)
-})
+db.on("connected", () => {
+  console.log("db is connected");
+});
+db.on("error", (error) => {
+  console.log(`db is getting ${error}`);
+});
 
-db.on("disconnected",()=>{
-    console.log("db is Disconnected")
-})
+db.on("disconnected", () => {
+  console.log("db is Disconnected");
+});
 
-module.exports =db
-
+module.exports = db;
